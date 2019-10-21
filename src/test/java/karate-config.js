7d4a65e539791b9fda55f02d079800249c1383b6
sevilay.agil <sevilayagil@gmail.com>
2019-10-22 00:07:09 +0300

    pom updated

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,8 +2,5 @@
   var config = {
     apiURL: 'http://dummy.restapiexample.com/api/v1/'
   };
-
-  karate.configure('connectTimeout', 5000);
-  karate.configure('readTimeout', 5000);
   return config;
 }
--- target/test-classes/karate-config.js
+++
@@ -1,9 +0,0 @@
- function() {
-  var config = {
-    apiURL: 'http://dummy.restapiexample.com/api/v1/'
-  };
-
-  karate.configure('connectTimeout', 5000);
-  karate.configure('readTimeout', 5000);
-  return config;
-}
